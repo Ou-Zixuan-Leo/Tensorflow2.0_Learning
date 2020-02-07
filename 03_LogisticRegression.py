a739f5e19d5c5ff545014bd74aeab96428ea34d1
Ou-Zixuan-Leo <56059253+Ou-Zixuan-Leo@users.noreply.github.com>
2020-02-07 11:31:52 +0800

    逻辑回归（二分类）例子
    输入变量具有15个维度，输出label为0或1

---
+++ 03_LogisticRegression.py
@@ -0,0 +1,35 @@
+# @FileName: Linear_Mul_nn.py
+# @Author : Ou Zixuan
+# @Time : 2020/2/6
+
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("dataset/credit-a.csv")
+data.head()
+
+x = data.iloc[:,:-1]
+y = data.iloc[:,-1].replace(-1,0)    #将-1的label替换成1
+
+
+
+model = tf.keras.Sequential([                                            #注意使用方括号
+        tf.keras.layers.Dense(8,input_shape=(15,),activation = 'relu'),  #输入层
+        tf.keras.layers.Dense(16,activation='relu'),                     #中间隐藏层 
+        tf.keras.layers.Dense(1,activation='sigmoid')                    #输出层
+])
+
+model.compile(optimizer = 'adam',
+              loss = 'binary_crossentropy',
+              metrics = ['acc']
+              
+)
+
+histiory = model.fit(x,y,epochs = 100)
+
+
+
+plt.plot(histiory.epoch,histiory.history.get('acc'))
+
